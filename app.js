92d2e2881a3bdfce71dfad6ba59e97a2c7743e3a
navas199565-rgb <navas199565@gmail.com>
2025-08-07 07:10:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ function addToCart(product) {
 }
 function updateCart() {
     document.getElementById("cart").innerHTML = "";
-    cart.forEach(item => {
+    cart.forEach(item => 
         let li = document.createElement("li");
         li.textContent = item;
         document.getElementById("cart").appendChild(li);
     });
     let message = "Hola, quiero comprar: " + cart.join(", ");
-    document.getElementById("whatsappLink").href = "https://wa.me/34644938677?text=" + encodeURIComponent(message);
-    window.open(url, "_blank");
-   };
-}
+    let = "https://wa.me/34644938677?text=" + encodeURIComponent(message);
+    document.getElementById("whatsappBtn").inclick = () => {
+      window.open(url, "_blank");
+    };
+  }
